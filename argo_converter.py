5afab854f245260c2b5cdf2a612e667d60e59991
himaganulakshmimedida-blip <himaganulakshmimedida@gmail.com>
2025-09-30 15:20:33 +0530

    Update argo_converter.py

--- argo_converter.py
+++ argo_converter.py
@@ -292,8 +292,8 @@ except Exception as e:
    print(f"Failed convert prof {fname}: {e}")
 def main(platform_ids: T.List[str]):
   print("Loading indexes...")
- idx_traj = load_index(INDEX_TRAJ)
- <br>local_nc = stream_download(url, local_nc)
+idx_traj = load_index(INDEX_TRAJ)
+local_nc = stream_download(url, local_nc)
 if not local_nc:
     print(f"⏭️ Skipping prof file: {fname}")
 continue
