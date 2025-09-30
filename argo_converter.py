fe62d4127c44e0421678a10ce13ec17691ca352b
himaganulakshmimedida-blip <himaganulakshmimedida@gmail.com>
2025-09-30 14:48:27 +0530

    Update argo_converter.py

--- argo_converter.py
+++ argo_converter.py
@@ -279,11 +279,11 @@ if not local_nc:
 for _, row in prof_files_sorted.tail(3).iterrows():
         url = row["url"]
 fname = row["filename"]
-        local_nc = os.path.join(DOWNLOAD_DIR, fname)
+ local_nc = os.path.join(DOWNLOAD_DIR, fname)
         print(f"Downloading prof: {url}")
         stream_download(url, local_nc)
         print(f"Converting prof: {local_nc}")
- try
+ try:
            df = convert_prof(local_nc)
           base = os.path.splitext(fname)[0]
           df.to_csv(os.path.join(out_dir, f"{base}_profiles.csv"),index=False)
